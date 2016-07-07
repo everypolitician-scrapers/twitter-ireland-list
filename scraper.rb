d06219b94b0b2ea27cf604a51709915719fac0ae
Dave Whiteland <dave@mysociety.org>
2016-07-07 17:15:21 +0100

    add some guessing of type of politician

--- scraper.rb
+++ scraper.rb
@@ -18,11 +18,32 @@ end
 # https://twitter.com/elainebyrne/lists/irish-politicians
 # remove prefix "Senator" and suffixed "TD" (Teachta Dála) from names
 
+# It might be handy to know if a name or handle indicates the type of
+# membership, so save it as guess_type.
+
+# "Sen" at the start of a string is a little bit risky, but
+# appears sound for current data.
+# "TD" and "MEP" at the end of the string seems safer.
+
+regexps = {
+  :td => /T\.?D\.?$/i,
+  :mep =>  /M\.?E\.?P\.?$/i,
+  :senator => /^Sen/i,
+}
+
 twitter.list_members('elainebyrne', 'irish-politicians').each do |person|
+  guess_type = '?'
+  regexps.each do |politician_type, regexp|
+    if person.name.match(regexps[politician_type]) || person.screen_name.match(regexps[politician_type])
+      guess_type = politician_type
+      break
+    end
+  end
   data = {
     id: person.id,
     name: person.name.gsub(/(^Sen(ator)?\s|\sT\.?D\.?$)/, ''),
     twitter: person.screen_name,
+    guess_type: guess_type.to_s
   }
   data[:image] = person.profile_image_url_https(:original).to_s unless person.default_profile_image?
   ScraperWiki.save_sqlite([:id], data)
